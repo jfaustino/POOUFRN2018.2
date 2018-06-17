204525b26a55d542dad5e6960ae2694066f73f4d
= <=>
2018-06-17 17:08:08 -0300

    Reorganização da classe Convidado.
    Manipulação de banco na Interface.

--- Convidado.py
+++ Convidado.py
@@ -1,20 +1,12 @@
-from hashlib import sha512
 from email.utils import parseaddr
-import datetime as dt
-import sqlite3
 
 class Convidado:
-	conn = sqlite3.connect("test.db")
-	cur = conn.cursor()
-
 	def __init__(self, email, local):
 		try:
 			self.Email = self.validaEmail(email)
 		except ValueError as e:
 			print("Algo de errado aconteceu. {}".format(e))
 		self.Local = local
-		curDate = dt.datetime.now(dt.timezone.utc).isoformat()
-		self.Id = sha512((self.Email + curDate).encode('utf-8')).hexdigest()
 
 	def validaEmail(self, email):
 		testEmail = parseaddr(email)[1]
@@ -23,10 +15,3 @@ class Convidado:
 			raise ValueError('E-mail inválido')
 
 		return testEmail
-	
-	def buscaLocal(self, local):
-		local = local.strip('\'\"\\;.')
-		queryLocal = "SELECT * FROM cities WHERE name LIKE %{}%;".format(local)
-		result = self.cur.execute(queryLocal)
-	
-		return result
--- Interface.py
+++ Interface.py
@@ -1,27 +1,71 @@
+from functools import partial
+import sqlite3
 import tkinter as tk
 
 class Application(tk.Frame):
+	conn = sqlite3.connect("test.db")
+	cur = conn.cursor()
+
 	def __init__(self, master=None):
 		super().__init__(master, height=600, width=800)
 		self.pack()
 		self.create_widgets()
 
 	def create_widgets(self):
-		self.fr1 = tk.Frame(self, width=400, height=480)
-		self.fr1.pack(side="left")
-		self.fr2 = tk.Frame(self, width=400, height=480)
-		self.fr2.pack(side="right")
-		self.hi_there = tk.Button(self)
-		self.hi_there["text"] = "Hello, world\n(click me)"
-		self.hi_there["command"] = self.say_hi
-		self.hi_there.pack()
+		# Rótulo principal
+		self.mainLabel = tk.Label(self, text="Selecione seu perfil")
+		self.mainLabel.pack()
 
-		self.quit = tk.Button(self, text="Quit", fg="red",
-			command=root.destroy)
-		self.quit.pack()
+		# Frames do anfitrião e do convidado
+		self.frAnfitriao = tk.Frame(self, width=400, height=480)
+		self.frAnfitriao.pack(side="left")
+		self.frConvidado = tk.Frame(self, width=400, height=480)
+		self.frConvidado.pack(side="right")
 
-	def say_hi(self):
-		print("Hi, there, everyone")
+		# Seleção de perfil
+		self.btAnfit = tk.Button(self.frAnfitriao, text="Anfitrião")
+		self.btAnfit["command"] = partial(self.selecionaPerfil, 0)
+		self.btAnfit.pack()
+		self.btConv = tk.Button(self.frConvidado, text="Convidado")
+		self.btConv["command"] = partial(self.selecionaPerfil, 1)
+		self.btConv.pack()
+
+	def selecionaPerfil(self, perfil):
+		self.frAnfitriao.pack_forget()
+		self.frConvidado.pack_forget()
+
+		if perfil == 0:
+			self.cur.execute("""CREATE TABLE IF NOT EXISTS eventos(
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				dtHoraInicio, dtHoraFim, anfitriao, localId""")
+
+			self.frEventos = tk.Frame(self, width=800, height=480)
+			self.frEventos.pack()
+
+			self.mainLabel["text"] = "Eventos"
+			self.btAdicEvento = tk.Button(self.frEventos,
+				command=partial(self.alteraEvento, "a"))
+			self.btEditarEvento = tk.Button(self.frEventos,
+				command=partial(self.alteraEvento, "e"))
+			self.btRemoverEvento = tk.Button(self.frEventos,
+				command=partial(self.alteraEvento, "r"))
+		elif perfil == 1:
+			self.frConvDetalhes = tk.Frame(self, width=800, height=480)
+			self.frConvDetalhes.pack()
+
+	def alteraEvento(self, acao):
+		# TODO
+		#if acao == "a":
+		#elif acao == "e":
+		#elif acao == "r":
+		return
+
+	def buscaLocal(self, local):
+		local = local.strip('\'\"\\;.')
+		queryLocal = "SELECT * FROM cities WHERE name LIKE %{}%;".format(local)
+		result = self.cur.execute(queryLocal)
+	
+		return result
 
 root = tk.Tk()
 app = Application(master=root)
